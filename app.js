85487bed952ace0a14dc0ca4fa62bc4ca328fc6a
Tigran Tigranian <ttigrani@ucsd.edu>
2014-04-05 22:54:17 -0700

    instagram app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //dependencies for each module used
-var express = require('express');
+var express = require('express'); 
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
@@ -10,6 +10,19 @@ var index = require('./routes/index');
 //database setup
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/instagramexample');
+//load environment variables
+var dotenv = require('dotenv');
+dotenv.load();
+//add instagram api setup
+var ig = require('instagram-node-lib');
+ig.set('client_id', process.env.instagram_client_id);
+ig.set('client_secret', process.env.instagram_client_secret);
+ig.tags.info({
+	name: "sushi",
+	complete: function(data) {
+		console.log(data);
+	}
+})
 
 //Configures the Template engine
 app.engine('handlebars', handlebars());
